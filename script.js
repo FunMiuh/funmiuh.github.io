a8fb27913f0360f52bff7081b13c7059a1e0c5cb
FunMiuh <ped.ruth.e@gmail.com>
2025-05-23 16:37:58 +0200

    removed bot

--- script.js
+++ script.js
@@ -30,37 +30,4 @@ function calculateTotal() {
   document.getElementById("total").textContent = `Total: ${total}€`;
 }
 
-document.querySelector(".chat-bubble").addEventListener("click", function (e) {
-    e.preventDefault();
-
-    const individualSelect = document.getElementById("individual");
-    const extrasSelect = document.getElementById("extras");
-
-    const individualText = individualSelect.options[individualSelect.selectedIndex].text;
-    const extrasText = extrasSelect.options[extrasSelect.selectedIndex].text;
-
-    const individualValue = parseInt(individualSelect.value) || 0;
-    const extrasValue = parseInt(extrasSelect.value) || 0;
-    const total = individualValue + extrasValue;
-
-    const message = `¡Hola! Me interesa un Funko personalizado.\n\n🧍 Opción: ${individualText}\n🎁 Extras: ${extrasText}\n💰 Precio total: ${total}€`;
-
-    fetch("https://api.telegram.org/bot7783243051:AAHm4zCiX9c0jHneCCMHSgF3kdH5Ke6RaXI/sendMessage", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            chat_id: 7534454788,
-            text: message
-        })
-    })
-    .then(() => {
-        alert("Mensaje enviado por Telegram 📩");
-    })
-    .catch(err => {
-        alert("Error al enviar mensaje 😢");
-        console.error(err);
-    });
-});
-
-
 setInterval(nextImage, 2000);
